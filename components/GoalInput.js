5ee31f17063d3ad8d00e606f386b4a75af85e0a5
Constantine <faustlarsen@gmail.com>
2021-03-10 17:51:42 -0700

    add delete button

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
-
-
-
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 
 const GoalInput = props => {
-  const [enteredGoal, setEnteredGoals] = useState('');
+  const [enteredGoal, setEnteredGoal] = useState('');
 
   const goalInputHandler = enteredText => {
     setEnteredGoal(enteredText);
   };
 
-  
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Course Goal"
-        style={styles.input}
-        onChangeText={goalInputHandler}
-        value={enteredGoal}
-      />
-      <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
-    </View>
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+      </View>
+    </Modal>
   );
 };
 
@@ -36,8 +34,7 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
-  },
-
-})
+  }
+});
 
 export default GoalInput;
